a5018c5003467637996bc0ec15049ab26db92f34
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-09-09 21:08:13 +0300

    subapps test

---
+++ tests/tests/11-sub-apps.js
@@ -0,0 +1,33 @@
+// must support sub-apps
+
+import express from "express";
+
+const app = express();
+const app2 = express();
+
+app.get('/test', (req, res) => {
+    res.send('test1');
+});
+    
+app2.get('/test', (req, res) => {
+    res.send('test2');
+});
+
+
+app.use('/asdf', app2);
+
+app.use((req, res, next) => {
+    res.send('404');
+});
+
+app.listen(13333, async () => {
+    console.log('Server is running on port 13333');
+
+    const outputs = await Promise.all([
+        fetch('http://localhost:13333/test').then(res => res.text()),
+        fetch('http://localhost:13333/asdf/test').then(res => res.text()),
+    ]);
+
+    console.log(outputs.join(' '));
+    process.exit(0);
+});
