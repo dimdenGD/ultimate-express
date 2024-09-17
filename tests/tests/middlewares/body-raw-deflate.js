1b50e21d0c72a410ee25475b75b1d85d070c8f4e
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-09-17 17:09:51 +0300

    add deflate test

---
+++ tests/tests/middlewares/body-raw-deflate.js
@@ -0,0 +1,43 @@
+// must support deflate raw body parser
+
+import express from "express";
+import bodyParser from "body-parser";
+import { deflate } from 'pako';
+
+const app = express();
+
+app.use(bodyParser.raw());
+
+app.post('/abc', (req, res) => {
+    res.send(req.body);
+});
+
+app.listen(13333, async () => {
+    console.log('Server is running on port 13333');
+    const ab = new ArrayBuffer(10);
+    const u8 = new Uint8Array(ab);
+    u8[0] = 1;
+    u8[1] = 2;
+    u8[2] = 3;
+    u8[3] = 4;
+    u8[4] = 5;
+    u8[5] = 6;
+    u8[6] = 7;
+    u8[7] = 8;
+    u8[8] = 9;
+
+    const response = await fetch('http://localhost:13333/abc', {
+        method: 'POST',
+        body: await deflate(ab),
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Content-Encoding': 'deflate'
+        }
+    });
+
+    const text = await response.text();
+    console.log(text);
+
+    process.exit(0);
+
+});
