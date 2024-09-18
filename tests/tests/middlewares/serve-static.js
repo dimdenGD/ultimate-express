a4d844d30e0048377a289726b6014fb73fba4cf9
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-09-18 20:44:57 +0300

    fix test

--- tests/tests/middlewares/serve-static.js
+++ tests/tests/middlewares/serve-static.js
@@ -23,10 +23,10 @@ app.listen(13333, async () => {
     console.log('Server is running on port 13333');
 
     const responses = await Promise.all([
-        // fetch('http://localhost:13333/abc'),
+        fetch('http://localhost:13333/abc'),
         fetch('http://localhost:13333/static/workers'),
-        // fetch('http://localhost:13333/static/index.js'),
-        // fetch('http://localhost:13333/static/../package.json'),
+        fetch('http://localhost:13333/static/index.js'),
+        fetch('http://localhost:13333/static/../package.json'),
     ]);
 
     const texts = await Promise.all(responses.map(r => r.text()));
