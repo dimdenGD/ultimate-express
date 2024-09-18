6d17036daaebadefc44ef9a4f7b8f0d359a23598
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-09-19 00:08:54 +0300

    express-session

---
+++ tests/tests/middlewares/express-session.js
@@ -0,0 +1,37 @@
+// must support express-session middleware
+
+const express = require("express");
+const session = require('express-session');
+
+const app = express();
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.get('/abc', (req, res) => {
+    req.session.views = (req.session.views || 0) + 1;
+    res.status(202).send(`Viewed ${req.session.views} times.`);
+});
+
+app.listen(13333, async () => {
+    console.log('Server is running on port 13333');
+
+    const response = await fetch('http://localhost:13333/abc');
+    const cookie = response.headers.get('Set-Cookie').match(/connect.sid=(.*?);/)[1];
+    const text = await response.text();
+    console.log(text, cookie, response.status);
+
+    const response2 = await fetch('http://localhost:13333/abc', {
+        headers: {
+            'Cookie': `connect.sid=${cookie}`
+        }
+    });
+    const text2 = await response2.text();
+    console.log(text2);
+
+    process.exit(0);
+
+});
