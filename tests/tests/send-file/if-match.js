fafe5abd2a33c124eee68299745c7d81bb554d68
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-09-20 06:18:07 +0300

    if-match test

---
+++ tests/tests/send-file/if-match.js
@@ -0,0 +1,31 @@
+// must support If-Match header
+
+const express = require("express");
+
+const app = express();
+
+app.get('/test', (req, res) => {
+    res.sendFile('src/index.js', { root: '.' });
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send(err.message);
+});
+
+app.listen(13333, async () => {
+    console.log('Server is running on port 13333');
+
+    const response = await fetch('http://localhost:13333/test', {
+        headers: {
+            'If-Unmodified-Since': new Date(0).toUTCString()
+        }
+    });
+    console.log(response.status, response.headers.get('ETag'));
+    const response2 = await fetch('http://localhost:13333/test', {
+        headers: {
+            'If-Match': '"' + response.headers.get('ETag') + '"'
+        }
+    });
+    console.log((await response2.text()).slice(0, 50), response2.status);
+    process.exit(0);
+});
