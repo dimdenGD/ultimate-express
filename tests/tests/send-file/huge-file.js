c2a64b4a4a9acc69cd66a51597ae5b53e4740231
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-09-27 01:50:24 +0300

    fix test

--- tests/tests/send-file/huge-file.js
+++ tests/tests/send-file/huge-file.js
@@ -13,8 +13,7 @@ app.get('/test', (req, res) => {
 app.listen(13333, async () => {
     console.log('Server is running on port 13333');
 
-    let start = Date.now();
     const response = await fetch('http://localhost:13333/test');
-    console.log(response.status, response.headers.get('Content-Length'), Date.now() - start);
+    console.log(response.status, response.headers.get('Content-Length'));
     process.exit(0);
 });
