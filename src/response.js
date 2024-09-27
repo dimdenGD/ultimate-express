5decc0f1e1e8bfa9bead0b974d46f6d055909d2d
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-09-27 12:46:27 +0300

    fix

--- src/response.js
+++ src/response.js
@@ -379,7 +379,7 @@ module.exports = class Response extends Writable {
         }
 
         // serve smaller files using workers
-        if(this.app.fsWorkers.length && stat.size < 1024 * 1024 && !ranged) {
+        if(this.app.workers.length && stat.size < 1024 * 1024 && !ranged) {
             this.app.readFileWithWorker(fullpath).then((data) => {
                 if(this._res.aborted) {
                     return;
