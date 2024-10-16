1ebc34d9068a32f33784a23c81b65f2945424378
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-10-16 23:17:32 +0300

    setting status after send

---
+++ tests/tests/res/status-twice.js
@@ -0,0 +1,18 @@
+// setting status after sending
+
+const express = require("express");
+
+const app = express();
+
+app.get('/test', (req, res) => {
+    res.status(200).send('test');
+    res.status(200);
+});
+
+app.listen(13333, async () => {
+    console.log('Server is running on port 13333');
+
+    const response = await fetch('http://localhost:13333/test');
+    console.log(await response.text());
+    process.exit(0);
+});
