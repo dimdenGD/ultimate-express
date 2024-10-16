148428d336585e71ebe55b55a5c486f836cee683
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-10-16 15:14:06 +0300

    fix disabling (#15)

---
+++ tests/tests/app/app-disable.js
@@ -0,0 +1,16 @@
+// must support disable
+
+const express = require("express");
+
+const app = express();
+app.disable('etag');
+
+app.get('/abc', (req, res) => {
+    res.send('abc');
+});
+
+app.listen(13333, async () => {
+    const output = await fetch('http://localhost:13333/abc');
+    console.log(output.headers.get('etag'));
+    process.exit(0);
+});
