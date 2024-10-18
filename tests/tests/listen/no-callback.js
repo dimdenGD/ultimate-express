b8e39702a4ecd4a9209d49a0ebe716ae02e4ec43
dimden <26517362+dimdenGD@users.noreply.github.com>
2024-10-18 21:49:18 +0300

    support no callback

---
+++ tests/tests/listen/no-callback.js
@@ -0,0 +1,12 @@
+// must support listen with no callback
+
+const express = require("express");
+
+const app = express();
+
+app.listen(13333);
+
+setTimeout(() => {
+    console.log('Server is running on port 13333');
+    process.exit(0);
+}, 500);
