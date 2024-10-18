49674ae2e61eb424685606bf6fcd72d160fd06bb
hillac <lachiehill46@gmail.com>
2024-10-18 14:35:55 +1000

    add types for settings

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,19 @@
 declare module 'ultimate-express' {
     import express from '@types/express';
+    import { AppOptions } from 'uWebSockets.js';
+    import { parse } from 'qs';
+    import { parse as fqsParse } from 'fast-querystring';
+    type Settings = {
+        uwsOptions?: AppOptions;
+        threads?: number;
+        "trust proxy fn"?: (ip: string, trust: string) => boolean;
+        "query parser fn"?: parse | fqsParse;
+        "view cache"?: boolean;
+        "views"?: string;
+        "etag fn"?: (body: any, econding: any) => string;
+        "case sensitive routing"?: boolean;
+        "strict routing"?: boolean;
+    }
+    export function express(settings?: Settings): ReturnType<typeof express>;
     export = express;
 }
