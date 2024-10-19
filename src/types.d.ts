dac168ddf4af16602fae5311c74b08d50b71db87
hillac <lachiehill46@gmail.com>
2024-10-20 00:05:33 +1000

    remove set types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,68 +1,49 @@
 declare module "ultimate-express" {
-  import express from "@types/express";
+  import e from "@types/express";
   import { AppOptions } from "uWebSockets.js";
-  import { parse } from "qs";
-  import { parse as fqsParse } from "fast-querystring";
 
   type Settings = {
     uwsOptions?: AppOptions;
     threads?: number;
   };
 
-  // Additional ultimate-express specific settings
-  type setOptions = {
-    "trust proxy fn"?: (ip: string, trust: string) => boolean;
-    "query parser fn"?: typeof fqsParse | typeof parse;
-    "view cache"?: boolean;
-    "etag fn"?: (body: any, econding: any) => string;
-    "case sensitive routing"?: boolean;
-    "strict routing"?: boolean;
-    [key: string]: any;
-  };
+  namespace express {
+    export import json = e.json;
+    export import raw = e.raw;
+    export import text = e.text;
 
-  interface UltimateExpress extends express.Express {
-    set<K extends keyof setOptions>(setting: K, value: setOptions[K]): this;
+    // export import application = e.application;
+    export import request = e.request;
+    export import response = e.response;
+
+    export import static = e.static;
+    // export import query = e.query;
+
+    export import urlencoded = e.urlencoded;
+
+    export import RouterOptions = e.RouterOptions;
+    export import Application = e.Application;
+    export import CookieOptions = e.CookieOptions;
+    export import Errback = e.Errback;
+    export import ErrorRequestHandler = e.ErrorRequestHandler;
+    export import Express = e.Express;
+    export import Handler = e.Handler;
+    export import IRoute = e.IRoute;
+    export import IRouter = e.IRouter;
+    export import IRouterHandler = e.IRouterHandler;
+    export import IRouterMatcher = e.IRouterMatcher;
+    export import MediaType = e.MediaType;
+    export import NextFunction = e.NextFunction;
+    export import Locals = e.Locals;
+    export import Request = e.Request;
+    export import RequestHandler = e.RequestHandler;
+    export import RequestParamHandler = e.RequestParamHandler;
+    export import Response = e.Response;
+    export import Router = e.Router;
+    export import Send = e.Send;
   }
 
-  namespace uexpress {
-    interface Express extends UltimateExpress {}
+  function express(settings?: Settings): e.Express;
 
-    export import json = express.json;
-    export import raw = express.raw;
-    export import text = express.text;
-    
-    // export import application = express.application;
-    export import request = express.request;
-    export import response = express.response;
-    
-    export import static = express.static;
-    // export import query = express.query;
-
-    export import urlencoded = express.urlencoded;
-    
-    export import RouterOptions = express.RouterOptions;
-    export import Application = express.Application;
-    export import CookieOptions = express.CookieOptions;
-    export import Errback = express.Errback;
-    export import ErrorRequestHandler = express.ErrorRequestHandler;
-    export import Express = express.Express;
-    export import Handler = express.Handler;
-    export import IRoute = express.IRoute;
-    export import IRouter = express.IRouter;
-    export import IRouterHandler = express.IRouterHandler;
-    export import IRouterMatcher = express.IRouterMatcher;
-    export import MediaType = express.MediaType;
-    export import NextFunction = express.NextFunction;
-    export import Locals = express.Locals;
-    export import Request = express.Request;
-    export import RequestHandler = express.RequestHandler;
-    export import RequestParamHandler = express.RequestParamHandler;
-    export import Response = express.Response;
-    export import Router = express.Router;
-    export import Send = express.Send;
-  }
-
-  declare function uexpress(settings?: Settings): UltimateExpress;
-
-  export = uexpress;
+  export = express;
 }
