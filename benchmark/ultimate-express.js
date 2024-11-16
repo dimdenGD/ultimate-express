e5599ee33b0195fb664f13f0fa6852cba5110db9
dimden <admin@dimden.dev>
2024-11-16 21:38:15 +0200

    Update ultimate-express.js

--- benchmark/ultimate-express.js
+++ benchmark/ultimate-express.js
@@ -2,6 +2,8 @@
 
 const express = require('../src/index');
 const app = express();
+app.set("etag", false);
+app.set("declarative responses", false);
 
 app.get('/', async (req, res) => res.send('Hello world'));
 app.listen(3000);
