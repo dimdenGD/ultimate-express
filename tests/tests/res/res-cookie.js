ab16aeed140facb376010a7fcfeeee3b45a5042d
Nigro Simone <nigro.simone@gmail.com>
2024-11-24 16:37:50 +0100

    fix: #68

--- tests/tests/res/res-cookie.js
+++ tests/tests/res/res-cookie.js
@@ -4,12 +4,20 @@ const express = require("express");
 
 const app = express();
 
+const sharedOptions = { maxAge: 1000 };
+
 app.get('/test', (req, res) => {
     res.cookie('test', '1');
     res.cookie('test2', '2', { maxAge: 1000 });
     res.cookie('test3', '3', { maxAge: 1000, path: '/test' });
     res.cookie('test4', '4', { maxAge: 1000, path: '/test', httpOnly: true });
     res.cookie('test5', '5', { maxAge: 1000, path: '/test', secure: true });
+
+    // see: https://github.com/dimdenGD/ultimate-express/issues/68
+    res.cookie('test6', '2', sharedOptions);
+    res.cookie('test6', '2', sharedOptions);
+    console.log(JSON.stringify(sharedOptions))
+
     res.send('test');
 });
 
