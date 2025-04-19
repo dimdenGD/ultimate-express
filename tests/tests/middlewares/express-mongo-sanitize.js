5993a3a52154271e1032d62914a6faa66e94ebb5
dimden <admin@dimden.dev>
2025-04-19 14:59:39 +0300

    add body to test

--- tests/tests/middlewares/express-mongo-sanitize.js
+++ tests/tests/middlewares/express-mongo-sanitize.js
@@ -5,10 +5,12 @@ const mongoSanitize = require("express-mongo-sanitize");
 
 const app = express();
 
+app.use(express.json());
 app.use(mongoSanitize());
 
-app.get('/abc', (req, res) => {
+app.post('/abc', (req, res) => {
     console.log(req.headers);
+    console.log(req.body);
     res.send('1');
 });
 
@@ -16,12 +18,20 @@ app.listen(13333, async () => {
     console.log('Server is running on port 13333');
 
     const response = await fetch('http://localhost:13333/abc', {
+        method: 'POST',
         headers: {
             'X-ad.test': '123',
             "$test": '4',
             "X-aa.bb": '5',
             'X-test': '6',
-        }
+            "content-type": 'application/json',
+        },
+        body: JSON.stringify({
+            abc: 123,
+            "$test": '4',
+            "X-aa.bb": '5',
+            'X-test': '6',
+        })
     });
     const text = await response.text();
     console.log(text);
