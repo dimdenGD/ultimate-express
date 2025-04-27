5fa5de5a448e027af1a6c1219e8f47962ba45e57
Nigro Simone <nigro.simone@gmail.com>
2025-04-27 20:47:54 +0200

    chore: double check response is compressed

--- tests/tests/middlewares/compression-big-file.js
+++ tests/tests/middlewares/compression-big-file.js
@@ -18,9 +18,14 @@ app.get('/abc', (req, res) => {
 app.listen(13333, async () => {
     console.log('Server is running on port 13333');
 
-    const response = await fetch('http://localhost:13333/large-file.json');
+    const response = await fetch('http://localhost:13333/large-file.json', {
+        method: 'GET',
+        headers: {
+            'Accept-Encoding': 'gzip',
+        },
+    });
     
-    console.log(response.headers.get('content-encoding'));
+    console.log(response.headers.get('content-encoding'), response.headers.get('content-encoding') === 'gzip');
     console.log(await response.json());
 
     process.exit(0);
