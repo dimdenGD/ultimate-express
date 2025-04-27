48bdf06fafffdec14f01e579283e8833e3d6c20f
Nigro Simone <nigro.simone@gmail.com>
2025-04-27 16:09:54 +0200

    chore: test del and delete

---
+++ tests/tests/app/app-delete.js
@@ -0,0 +1,25 @@
+// must support app.delete() and app.del()
+
+const express = require("express");
+
+const app = express();
+
+app.delete('/delete', (req, res) => {
+    res.send('delete');
+});
+
+app.del('/del', (req, res) => {
+    res.send('del');
+});
+
+app.listen(13333, async () => {
+    console.log('Server is running on port 13333');
+
+    const outputs = await Promise.all([
+        fetch('http://localhost:13333/delete', {method: 'DELETE'}).then(res => res.text()),
+        fetch('http://localhost:13333/del', {method: 'DELETE'}).then(res => res.text()),
+    ]);
+
+    console.log(outputs);
+    process.exit(0);
+});
