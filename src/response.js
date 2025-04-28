cddab92558c818c8940369f499e0c24b5dfb2fcd
dimden <admin@dimden.dev>
2025-04-28 21:40:09 +0300

    Fix #143

--- src/response.js
+++ src/response.js
@@ -292,9 +292,8 @@ module.exports = class Response extends Writable {
             
             this.finished = true;
             if(this.socketExists) this.socket.emit('close');
+            super.end();
         });
-
-        this.emit('finish')
         return this;
     }
 
