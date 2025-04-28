4a000009f4b94d9ed896f5e45475dc003d393bcf
Nigro Simone <nigro.simone@gmail.com>
2025-04-28 08:48:10 +0200

    chore(test): add missing case of read param into json body

--- tests/tests/req/req-param.js
+++ tests/tests/req/req-param.js
@@ -4,6 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.set('body methods', ['POST', 'PUT', 'PATCH', 'DELETE']);
+app.use(express.json());
+
+app.delete('/delete', (req, res) => {
+    console.log(req.param('test'));
+    console.log(req.param('test', 'default'));
+    res.send('post');
+});
+
 app.all('/test', (req, res) => {
     console.log(req.param('test'));
     console.log(req.param('test', 'default'));
@@ -19,11 +28,26 @@ app.get('/test/:test', (req, res) => {
 app.listen(13333, async () => {
     console.log('Server is running on port 13333');
 
-    await fetch('http://localhost:13333/test').then(res => res.text());
+    await fetch('http://localhost:13333/delete', {
+        method: 'DELETE', 
+        body: JSON.stringify({test: 'aaa'}), 
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }).then(res => res.text());
+    await fetch('http://localhost:13333/delete', {
+        method: 'DELETE', 
+        body: JSON.stringify({}), 
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }).then(res => res.text());
+
     await fetch('http://localhost:13333/test?test=test').then(res => res.text());
     await fetch('http://localhost:13333/test?test=test&test2=test2').then(res => res.text());
     await fetch('http://localhost:13333/test?asdf').then(res => res.text());
     await fetch('http://localhost:13333/test/test').then(res => res.text());
     await fetch('http://localhost:13333/test/test/test').then(res => res.text());
+
     process.exit(0);
 });
