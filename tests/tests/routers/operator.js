d6ca8b2726e00becb7f9150ff3505c5198d43b14
Nigro Simone <nigro.simone@gmail.com>
2025-04-29 19:23:42 +0200

    chore: test routing (very hard)

--- tests/tests/routers/operator.js
+++ tests/tests/routers/operator.js
@@ -8,26 +8,27 @@ const router = express.Router();
 
 // This route path will match acd and abcd.
 router.get("/ab?cd", (req, res) => {
-  res.send("ab?cd -> " + req.path);
+  res.json({ test: "ab?cd", path: req.path });
 });
 
 // This route path will match abcd, abbcd, abbbcd, and so on.
 router.get("/ab+cd", (req, res) => {
-  res.send("ab+cd -> " + req.path);
+  res.json({ test: "ab+cd", path: req.path, param: req.params });
 });
 
 // This route path will match abcd, abxcd, abRANDOMcd, ab123cd, and so on.
 router.get("/ab*cd", (req, res) => {
-  res.send("ab*cd -> " + req.path);
+  res.json({ test: "ab*cd", path: req.path, param: req.params });
 });
 
 // This route path will match /abe and /abcde.
 router.get("/ab(cd)?e", (req, res) => {
-  res.send("ab(cd)?e -> " + req.path);
+  res.json({ test: "ab(cd)?e", path: req.path, param: req.params });
 });
 
 app.use(router);
-app.use('/sub', router);
+app.use("/sub", router);
+app.use("/sub/:id", router);
 
 app.listen(13333, async () => {
   console.log("Server is running on port 13333");
@@ -42,12 +43,23 @@ app.listen(13333, async () => {
     "abcde",
   ];
   const responses1 = await Promise.all(
-    parts.map((part) => fetch(`http://localhost:13333/${part}`).then((res) => res.text()))
+    parts.map((part) =>
+      fetch(`http://localhost:13333/${part}`).then((res) => res.json())
+    )
   );
   const responses2 = await Promise.all(
-    parts.map((part) => fetch(`http://localhost:13333/sub/${part}`).then((res) => res.text()))
+    parts.map((part) =>
+      fetch(`http://localhost:13333/sub/${part}`).then((res) => res.json())
+    )
+  );
+  const responses3 = await Promise.all(
+    parts.map((part) =>
+      fetch(`http://localhost:13333/sub/123/${part}`).then((res) => res.json())
+    )
   );
   console.log(responses1);
   console.log(responses2);
+  console.log(responses3);
+
   process.exit(0);
 });
