8f3c08a07c9f83884056f6394b6ae6cea4da29d0
Nigro Simone <nigro.simone@gmail.com>
2025-04-30 19:42:46 +0200

    chore: test error response was already sent

---
+++ tests/tests/res/errors.js
@@ -0,0 +1,27 @@
+// must support res error response was already sent
+
+const express = require("express");
+
+const app = express();
+
+app.set("declarative responses", false);
+
+app.get("/test", (req, res) => {
+  res.send("Hello World");
+  res.send("Hello World"); // Response was already sent!
+});
+
+app.use((err, req, res, next) => {
+  console.log(typeof err?.message === "string"); // just check if there is an error
+});
+
+app.listen(13333, async () => {
+  console.log("Server is running on port 13333");
+
+  const responses = await fetch("http://localhost:13333/test").then((res) =>
+    res.text()
+  );
+
+  console.log(responses);
+  process.exit(0);
+});
