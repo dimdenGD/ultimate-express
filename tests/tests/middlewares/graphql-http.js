4c4743c2ede99b8c3163bfa79355ab20d91638e1
Nigro Simone <nigro.simone@gmail.com>
2025-05-01 13:48:13 +0200

    chore: test graphql-http

---
+++ tests/tests/middlewares/graphql-http.js
@@ -0,0 +1,38 @@
+const express = require('express'); // yarn add express
+const { createHandler } = require('graphql-http/lib/use/express');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve: () => 'world',
+      },
+    },
+  }),
+});
+
+
+const app = express();
+app.all('/graphql', createHandler({ schema }));
+
+app.listen(13333, async () => {
+    console.log("Server is running on port 13333");
+
+    const response = await fetch('http://localhost:13333/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: '{ hello }',
+        }),
+    })
+    .then(res => res.json())
+
+    console.log(response);
+
+    process.exit(0);
+});
