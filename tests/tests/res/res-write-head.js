fd51fddb549076a9d284106bf9a9f9d249bcc567
Nigro Simone <nigro.simone@gmail.com>
2025-05-03 12:49:17 +0200

    fix(test): double check writeHead return this

--- tests/tests/res/res-write-head.js
+++ tests/tests/res/res-write-head.js
@@ -5,21 +5,19 @@ const express = require("express");
 const app = express();
 
 app.get('/test-1', (req, res) => {
-    res.writeHead(201).end();
+    res.writeHead(201).end('test-1');
 });
 
 app.get('/test-2', (req, res) => {
     res.writeHead(202, {
         'x-test': '2'
-    });
-    res.end();
+    }).end('test-2');
 });
 
 app.get('/test-3', (req, res) => {
     res.writeHead(203, 'test-3', {
         'x-test': '3'
-    });
-    res.end();
+    }).end('test-3');
 });
 
 
@@ -32,11 +30,14 @@ app.listen(13333, async () => {
         fetch('http://localhost:13333/test-3')
     ])
 
-    console.log(response.map(res => ({
-        status: res.status,
-        header: res.headers['x-test'],
-        statusText: res.statusText
-    })));
+    for (const res of response) {
+        console.log({
+            status: res.status,
+            header: res.headers['x-test'],
+            statusText: res.statusText,
+            body: await res.text()
+        });
+    }
 
     process.exit(0)
 });
