b9b5e7316d26c3f2a01bd624b7cc03a7a9d28607
dimden <admin@dimden.dev>
2025-05-03 22:33:14 +0300

    test more router function

--- tests/tests/routers/router-function.js
+++ tests/tests/routers/router-function.js
@@ -13,11 +13,16 @@ app.use((req, res, next) => {
     router(req, res, next);
 });
 
+app.get('/test2', (req, res) => {
+    res.send('test2');
+});
+
 app.listen(13333, async () => {
     console.log('Server is running on port 13333');
 
     let output1 = await fetch('http://localhost:13333/test').then(res => res.text());
+    let output2 = await fetch('http://localhost:13333/test2').then(res => res.text());
 
-    console.log(output1);
+    console.log(output1, output2);
     process.exit(0);
 });
