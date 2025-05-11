7f5fa4cce0d821422308d0c375d0271d7a5c5b4b
Nigro Simone <nigro.simone@gmail.com>
2025-05-11 10:42:46 +0200

    fix: same type

--- tests/utils.js
+++ tests/utils.js
@@ -9,7 +9,13 @@ async function fetchTest(input, init) {
   console.log("status", response.status);
   console.log("statusText", response.statusText);
 
-  console.log("content-type", response.headers.get("content-type")?.toLowerCase());
+  let contentType = response.headers.get("content-type")?.toLowerCase();
+  // ultimate use different mime type resolver
+  if( contentType?.includes('text/javascript') ){
+    contentType = contentType.replace('text/javascript', 'application/javascript')
+  }
+  console.log("content-type", contentType);
+  
   console.log("content-encoding", response.headers.get("content-encoding"));
   console.log("vary", response.headers.get("vary"));
   console.log("set-cookie", typeof response.headers.get("set-cookie"));
