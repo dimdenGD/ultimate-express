4df1ab0de281ce736bdd2302efe1e251a67c7eae
Nigro Simone <nigro.simone@gmail.com>
2026-04-12 16:29:54 +0200

    fix: uws has different timing on listen/callback

--- tests/tests/app/close.js
+++ tests/tests/app/close.js
@@ -4,11 +4,20 @@ const express = require("express");
 
 const app = express();
 
-const server = app.listen(13333, () => {
+const server = app.listen(13333, (err) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
     console.log("Server is listening on port 13333");
 });
 
-server.close(() => {
-    console.log("Server closed successfully");
-    process.exit(0);
-})
+setTimeout(() => {
+    server.close((err) => {
+        console.log("Server closed successfully");
+        if(err){
+            console.log(err);
+        }
+        process.exit(err ? 1 : 0)
+    })
+}, 1000);
